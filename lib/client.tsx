cec0372c6e5f4690a9ca12416128f8c5b3e0a201
Abhinav Anand <abhinav.anand500@gmail.com>
2025-04-24 12:50:28 +0530

    Deploying

--- lib/client.tsx
+++ lib/client.tsx
@@ -5,7 +5,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-08-18",
-  useCdn: true,
+  useCdn: false,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
 
